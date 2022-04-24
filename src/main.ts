0af9774f6e709a8c179df39e4bd916b834105536
许杰 <384977866@qq.com>
2022-04-24 20:18:18 +0800

    add user operation html

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementPlus from "element-plus";
+import "element-plus/theme-chalk/index.css";
+import * as ELIcons from '@element-plus/icons-vue'
 
-createApp(App).use(router).mount('#app')
+
+
+
+// createApp(App).use(router).use(ElementPlus).mount("#app");
+const app = createApp(App);
+for (const name in ELIcons) {
+    app.component(name,(ELIcons as any)[name]);
+}
+app.use(router).use(ElementPlus).mount("#app");
--- src/views/indexUser.vue
+++ src/views/indexUser.vue
@@ -3,7 +3,47 @@
     <div class="head">
       <div class="lift"></div>
       <div class="main">
-        
+        <div class="head_main_lift"></div>
+        <div class="head_main_main">
+          <el-row class="mb-4">
+            <el-button
+              size="small"
+              type="info"
+              plain
+              style="margin-left: 20px; border: none"
+              >优惠</el-button
+            >
+          </el-row>
+          <el-row class="mb-4">
+            <el-button
+              size="small"
+              type="info"
+              plain
+              style="margin-left: 20px; border: none"
+              >玩法规则</el-button
+            >
+          </el-row>
+          <el-row class="mb-4">
+            <el-button
+              size="small"
+              type="info"
+              plain
+              style="margin-left: 20px; border: none"
+              >代理</el-button
+            >
+          </el-row>
+          <el-row class="mb-4">
+            <el-button
+              size="small"
+              type="info"
+              plain
+              style="margin-left: 20px; border: none"
+              >手机版/APP</el-button
+            >
+          </el-row>
+          <el-icon style="margin-right: 20px;"><apple /></el-icon>
+        </div>
+        <div class="head_main_right"></div>
       </div>
       <div class="right"></div>
     </div>
@@ -14,6 +54,8 @@
     </div>
   </div>
 </template>
+<script lang="ts" setup>
+</script>
 <style lang="scss">
 * {
   /* 初始化 取消页面的内边距 */
@@ -32,22 +74,36 @@
   width: 100%;
   height: 5%;
   display: flex;
-  background-color: black;
+  border: 1px solid red;
 }
-.boby{
+.boby {
   width: 100%;
   height: 95%;
   display: flex;
 }
 .lift {
-  width: 15%;
+  width: 16%;
   background-color: rgb(114, 109, 109);
 }
 .main {
-  width: 70%;
+  width: 74%;
+  display: flex;
 }
 .right {
-  width: 15%;
+  width: 10%;
   background-color: rgb(114, 109, 109);
+}
+.head_main_lift {
+  width: 10%;
+  background-color: aqua;
+}
+.head_main_main {
+  width: 74%;
+  display: flex;
+  align-items: center; // 垂直对齐
+}
+.head_main_right {
+  width: 16%;
+  background-color: aqua;
 }
 </style>
